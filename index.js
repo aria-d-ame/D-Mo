cc89771ed09e45e52a308dec1274d5c5e134ed3c
Aria's Laptop <aria.d.ame233@gmail.com>
2026-09-25 17:40:58 -0400

    0.1.2 - MongoDB now works

--- index.js
+++ index.js
@@ -2,37 +2,36 @@ const { GClient, Plugins, Command, Component } = require('gcommands');
 const { GatewayIntentBits, Partials } = require('discord.js');
 const { join } = require('path');
 require('dotenv').config();
-// import("mongoose");
-// const mongoose = require('mongoose');
+import("mongoose");
+const mongoose = require('mongoose');
 
 Command.setDefaults({
 	cooldown: '10s',
 });
 
-// Set the default onError function for components
+// Set the default error function
 Component.setDefaults({
     onError: (ctx, error) => {
-        return ctx.reply('Oh no! Something when wrong!')
+        return ctx.reply('Oh no! Something went wrong!')
     }
 });
 
-// Search for plugins in node_modules (folder names starting with gcommands-plugin-) or plugins folder
+// Search for plugins in node_modules
 Plugins.search(__dirname);
 
 const client = new GClient({
-    // Register the directories where your commands/components/listeners will be located.
+    // Register directories
     dirs: [
         join(__dirname, 'commands'),
         join(__dirname, 'components'),
         join(__dirname, 'listeners')
     ],
-    // Enable message support
+    // Enables message support
     messageSupport: true,
-    // Set the prefix for message commands
-    messagePrefix: 'c.',
-    // Set the guild where you will be developing your bot. This is useful because guild slash commands update instantly.
+    // Sets prefix for message commands
+    messagePrefix: 'd-',
     devGuildId: process.env.devGuildId,
-    // Set the intents you will be using (https://discordjs.guide/popular-topics/intents.html#gateway-intents)
+    // Sets intents 
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -43,23 +42,20 @@ const client = new GClient({
     partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.User, Partials.Reaction]
 });
 
-//Connects mongoDB
-// (async function connect() {
-//    mongoose.set('strictQuery', false);
-//    try {
-//        console.log(`🔄 Connecting database...`);
-//
-//        await mongoose.connect(process.env.MONGO_TOKEN,);
-//    } catch (error) {
-//        console.log(`Error ${error}`);
-//        console.log("⚠️ Database did not connect!");
-//    }
-//})();
- 
-//Logs mongoDB connect
-//mongoose.connection.once("open", () => {
-//    console.log("✅ Database connected successfully!");
-//});
+// Connects mongoDB
+ (async function connect() {
+    mongoose.set('strictQuery', false);
+    try {
+        console.log(`🔄 Connecting to MongoDB...`);
+
+        await mongoose.connect(process.env.mongoToken,);
+
+        console.log(`✅ MongoDB connected successfully!`);
+    } catch (error) {
+        console.log(`Error ${error}`);
+        console.log("⚠️ MongoDB did not connect!");
+    }
+})();
 
 // Login to the discord API
-client.login(process.env.BOT_TOKEN,).catch(console.error);
+client.login(process.env.botToken,).catch(console.error);
